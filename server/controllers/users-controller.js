79f36ec873d848d7b722a5510f6c38b91b2e21ad
Dhanw <jose500daniel@gmail.com>
2023-10-05 21:31:27 -0600

    login is ready, I need to check it later when add Security also. Last, need to return all the users

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -23,35 +23,33 @@ const getUsers = (req, res, next) => {
 };
 
 const signup = async (req, res, next) => {
-
   const error = validationResult(req);
-  
+
   if (!error.isEmpty()) {
     console.log(error);
-     next(new HttpError("invalid inputs please try again", 422));
+    next(new HttpError("invalid inputs please try again", 422));
   }
 
-  const { name, email, password ,places} = req.body;
+  const { name, email, password, places } = req.body;
   let existingUser;
   try {
-     existingUser = await User.findOne({email:email})
+    existingUser = await User.findOne({ email: email });
   } catch (erro) {
     return next(new HttpError("Sign up failed", 422));
   }
 
-
-  
-  if(existingUser){
+  if (existingUser) {
     const error = new HttpError("Exisitng user, please ologin instead", 422);
     return next(error);
   }
-  
+
   const createdUser = new User({
     name,
     email,
-    image:'https://www.shutterstock.com/shutterstock/photos/2275817317/display_1500/stock-photo-happy-man-standing-on-road-in-front-of-trees-2275817317.jpg',
+    image:
+      "https://www.shutterstock.com/shutterstock/photos/2275817317/display_1500/stock-photo-happy-man-standing-on-road-in-front-of-trees-2275817317.jpg",
     password,
-    places
+    places,
   });
 
   try {
@@ -61,20 +59,29 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-  
-  res.json({ user: createdUser.toObject({getters:true})});
+  res.json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
+  const error = validationResult(req);
   const { email, password } = req.body;
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
 
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError(
-      "Could not identified  user, credentials or email seems to be wrong",
-      401
-    );
+  if (!error.isEmpty()) {
+    console.log(error);
+    next(new HttpError("invalid inputs please try again", 422));
   }
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (erro) {
+    return next(new HttpError("Sign up failed", 422));
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError("Invalid credentialas  user", 422);
+    return next(error);
+  }
+
 
   res.json({ message: "Logged in" });
 };
